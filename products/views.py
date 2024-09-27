e629fdf12a5043cdcdfcd4893099da0c9f4b8475
rsof14 <sofia.rytik@gmail.com>
2024-09-27 13:40:38 +0300

    First version without ajax

--- products/views.py
+++ products/views.py
@@ -1,19 +1,29 @@
+from django.shortcuts import redirect
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.renderers import TemplateHTMLRenderer
 from .models import Product
 from .serializers import ProductSerializer
 
 
 class ProductsAPIView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'index.html'
+
     def get(self, request):
         products = Product.objects.all()
-        return Response({'products': ProductSerializer(products, many=True).data})
+        serializer = ProductSerializer()
+        return Response({'serializer': serializer, 'products': ProductSerializer(products, many=True).data})
 
     def post(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
+        if not serializer.is_valid():
+            return Response({'serializer': serializer, 'product': serializer.data})
 
-        return Response({'product': serializer.data})
+        serializer.save()
+        return redirect(request.path_info)
+
+
 
 
