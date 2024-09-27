3fee1dc4d53fb71b87b29233946e87cb09694081
rsof14 <sofia.rytik@gmail.com>
2024-09-27 17:08:34 +0300

    Second version with ajax

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import redirect
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.renderers import TemplateHTMLRenderer
@@ -12,17 +14,16 @@ class ProductsAPIView(APIView):
 
     def get(self, request):
         products = Product.objects.all()
-        serializer = ProductSerializer()
-        return Response({'serializer': serializer, 'products': ProductSerializer(products, many=True).data})
+        return Response(data={'products': ProductSerializer(products, many=True).data}, status=status.HTTP_200_OK)
 
     def post(self, request):
         serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        if not serializer.is_valid():
-            return Response({'serializer': serializer, 'product': serializer.data})
-
-        serializer.save()
-        return redirect(request.path_info)
+        try:
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(data={'product': serializer.data}, status=status.HTTP_201_CREATED)
+        except ValidationError:
+            return JsonResponse({'message': 'Название продукта должно быть уникальным'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
